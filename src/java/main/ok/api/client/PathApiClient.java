c4b2f65a32181b596ae8c0a1dbc5304f2b64788d
artsidorenko <art.sidorenko2@gmail.com>
2023-12-18 03:40:07 +0300

    remove idea specific files; add some tests to http client; add basic current user test

--- src/java/main/ok/api/client/PathApiClient.java
+++ src/java/main/ok/api/client/PathApiClient.java
@@ -1,21 +1,20 @@
 package ok.api.client;
 
 import ok.api.config.HttpClientConfig;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 
 public class PathApiClient extends AbstractHttpClient {
     public PathApiClient(HttpClientConfig config) {
-        super(config, "/api/ok");
+        super(config, "/api");
     }
 
     @Override
-    public HttpUriRequest buildRequest(String restMethod, String methodGroup, String methodName, Map<String, String> parameters) {
-        RequestBuilder builder = RequestBuilder.create(restMethod)
-                .setUri(path + '/' + methodGroup + '/' + methodName);
+    public RequestBuilder buildRequest(@NotNull String restMethod, @NotNull String methodGroup, @NotNull String methodName, @NotNull Map<String, String> parameters) {
+        RequestBuilder builder = RequestBuilder.create(restMethod).setUri(path + '/' + methodGroup + '/' + methodName);
         parameters.forEach(builder::addParameter);
-        return builder.build();
+        return builder;
     }
 }
