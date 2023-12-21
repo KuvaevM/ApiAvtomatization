206ac5f6adb98918662be2ba96ac3608b7d9ab12
artsidorenko <art.sidorenko2@gmail.com>
2023-12-21 16:30:20 +0300

    add response deserialization and success sceario on getCurrentUser

--- src/java/test/ok/api/users/get/TestGetCurrentUser.java
+++ src/java/test/ok/api/users/get/TestGetCurrentUser.java
@@ -1,16 +1,20 @@
 package ok.api.users.get;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import ok.api.client.MethodApiClient;
 import ok.api.client.PathApiClient;
 import ok.api.config.HttpClientConfig;
+import ok.api.util.TestUtil;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Map;
 
 import static ok.api.users.get.ApplicationSecrets.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ok.api.util.TestUtil.MAPPER;
+import static ok.api.util.TestUtil.tryExtractError;
+import static org.junit.jupiter.api.Assertions.*;
 
 // https://apiok.ru/dev/methods/rest/users/users.getCurrentUser
 public class TestGetCurrentUser {
@@ -18,7 +22,6 @@ public class TestGetCurrentUser {
     private final HttpClientConfig config = HttpClientConfig.loadFromResources();
     private final PathApiClient pathClient = new PathApiClient(config);
     private final MethodApiClient methodClient = new MethodApiClient(config);
-    private final ObjectMapper MAPPER = new ObjectMapper();
 
     public TestGetCurrentUser() throws IOException {
     }
@@ -29,27 +32,49 @@ public class TestGetCurrentUser {
     public void testPathApi() throws IOException {
         pathClient.setApplication(APPLICATION_ID, APPLICATION_KEY, APPLICATION_SECRET_KEY);
         pathClient.setCredentials(ACCESS_TOKEN, SESSION_SECRET);
+
         var requestBuilder = pathClient.buildRequest("GET", "users", "getCurrentUser", Map.of());
         pathClient.addRequestApplication(requestBuilder);
         pathClient.addRequestAccessToken(requestBuilder);
         pathClient.signRequest(requestBuilder);
         var response = pathClient.execute(requestBuilder.build());
         assertEquals(200, response.getStatusLine().getStatusCode());
-//        TODO: deserialize response
-//        assertEquals(UID, MAPPER.convertValue(response.getEntity(), GetCurrentUserResponse.class).UID);
+
+        var contentStr = TestUtil.readContentToString(response);
+        var error = tryExtractError(contentStr);
+        assertNull(error);
+
+        var currentUser = MAPPER.readValue(contentStr, GetCurrentUserResponse.class);
+        assertNotNull(currentUser);
+        assertNotNull(currentUser.UID);
+        assertEquals(UID, currentUser.UID);
     }
-
-
 
     @Test
     public void testMethodApi() throws IOException {
-        pathClient.setApplication(APPLICATION_ID, APPLICATION_KEY, APPLICATION_SECRET_KEY);
-        pathClient.setCredentials(ACCESS_TOKEN, SESSION_SECRET);
-        var request = pathClient.buildRequest("GET", "users", "getCurrentUser", Map.of()).build();
-        pathClient.execute(request);
+        methodClient.setApplication(APPLICATION_ID, APPLICATION_KEY, APPLICATION_SECRET_KEY);
+        methodClient.setCredentials(ACCESS_TOKEN, SESSION_SECRET);
+
+        var requestBuilder = methodClient.buildRequest("GET", "users", "getCurrentUser", Map.of());
+        methodClient.addRequestApplication(requestBuilder);
+        methodClient.addRequestAccessToken(requestBuilder);
+        methodClient.signRequest(requestBuilder);
+        var response = methodClient.execute(requestBuilder.build());
+        assertEquals(200, response.getStatusLine().getStatusCode());
+
+        var contentStr = TestUtil.readContentToString(response);
+        var error = tryExtractError(contentStr);
+        assertNull(error);
+
+        var currentUser = MAPPER.readValue(contentStr, GetCurrentUserResponse.class);
+        assertNotNull(currentUser);
+        assertNotNull(currentUser.UID);
+        assertEquals(UID, currentUser.UID);
     }
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
     private static class GetCurrentUserResponse {
+        @JsonProperty("uid")
         public String UID;
     }
 }
