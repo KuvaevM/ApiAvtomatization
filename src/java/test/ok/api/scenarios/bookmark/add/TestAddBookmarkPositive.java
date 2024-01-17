15ed6ab4b6a2b38c46fa6fb4ea4268e278b54607
artsidorenko <art.sidorenko2@gmail.com>
2024-01-17 23:26:33 +0300

    reformat code; add links to methods docs

--- src/java/test/ok/api/scenarios/bookmark/add/TestAddBookmarkPositive.java
+++ src/java/test/ok/api/scenarios/bookmark/add/TestAddBookmarkPositive.java
@@ -15,6 +15,7 @@ import static ok.api.util.TestUtil.MAPPER;
 import static ok.api.util.TestUtil.tryExtractError;
 import static org.junit.jupiter.api.Assertions.*;
 
+// https://apiok.ru/dev/methods/rest/bookmark/bookmark.add
 public class TestAddBookmarkPositive extends AbstractTest {
     @ParameterizedTest
     @MethodSource("provideHttpClients")
@@ -22,9 +23,7 @@ public class TestAddBookmarkPositive extends AbstractTest {
         client.setApplication(applicationSecrets.applicationId(), applicationSecrets.applicationKey(), applicationSecrets.applicationSecretKey());
         client.setCredentials(applicationSecrets.accessToken(), applicationSecrets.sessionSecret());
 
-        var requestBuilder = client.buildRequest("GET", "bookmark", "add",
-                Map.of("ref_id", "53038939046008",
-                        "bookmark_type", "GROUP"));
+        var requestBuilder = client.buildRequest("GET", "bookmark", "add", Map.of("ref_id", "53038939046008", "bookmark_type", "GROUP"));
         client.addRequestApplication(requestBuilder);
         client.addRequestAccessToken(requestBuilder);
         client.signRequest(requestBuilder);
